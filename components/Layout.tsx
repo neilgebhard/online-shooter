7ba47fc4a5b7979c91a16aff02f863b39c9a00d8
Neil Gebhard <neilgebhard@gmail.com>
2022-05-11 00:08:10 +1000

    :construction: Add next-auth scaffolding

---
+++ components/Layout.tsx
@@ -0,0 +1,14 @@
+import Navbar from './Navbar'
+
+type Props = {
+  children: React.ReactNode
+}
+
+export default function Layout({ children }: Props) {
+  return (
+    <>
+      <Navbar />
+      {children}
+    </>
+  )
+}
