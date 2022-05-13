e469952dab1b3483617c42f2a91ca0840e66d96c
Neil Gebhard <neilgebhard@gmail.com>
2022-05-13 15:34:37 +1000

    :sparkles: Add prisma

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,10 @@
 import NextAuth from 'next-auth'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import GoogleProvider from 'next-auth/providers/google'
+import { prisma } from '../../../db'
 
 export default NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID!,
